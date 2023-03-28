010e93a778585a5475ce5fb91fd9627a184c8890
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-03-28 12:10:43 -0700

    Finished Task 3

--- index.js
+++ index.js
@@ -55,9 +55,17 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, cb) {
+    const filtered = cb(array)
+    const years = []
+    filtered.map((game) => {
+        return years.push(game.Year)
+    })
+    
+    return years
 }
+
+console.log(`Task 3:`, getYears(fifaData, getFinals))
 
 
 
